8d7cdc8bbdde5605bc7c61a9cb803fa645451495
maringan <kamil.kucharski88+1@gmail.com>
2014-06-12 01:56:42 +0200

    working

--- test/smsapi_spec.js
+++ test/smsapi_spec.js
@@ -1,6 +1,10 @@
-// var should = require('chai').should(),
-//     smsApi = require('../index')
-//
-// describe("", function(){
-//
-// })
+var should = require('chai').should(),
+    smsApi = require('../index')
+
+describe('SmsApi', function(){
+  describe("#sendSms", function(){
+    it("should return response from sms api", function(){
+      // to test it !!
+    })
+  })
+})
--- test/url_helper_spec.js
+++ test/url_helper_spec.js
@@ -6,14 +6,16 @@ var should = require('chai').should(),
     urlInstance = new UrlHelper(url, testHash),
     paramsToString = urlInstance.paramsToString
 
-describe("#paramsToString", function(){
-  it("should change hash to string", function(){
-    paramsToString(testHash).should.eql("?a=1&&b=2")
+describe("UrlHelper", function(){
+  describe("#paramsToString", function(){
+    it("should change hash to string", function(){
+      paramsToString(testHash).should.eql("?a=1&b=2")
+    })
   })
-})
 
-describe("#buildUrlWithParams", function(){
-  it("should build correct url with params", function(){
-    urlInstance.buildUrlWithParams().should.eql("http://xyz.com?a=1&&b=2")
+  describe("#buildUrlWithParams", function(){
+    it("should build correct url with params", function(){
+      urlInstance.buildUrlWithParams().should.eql("http://xyz.com/?a=1&b=2")
+    })
   })
 })
